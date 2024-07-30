c67709fb19ecb1db628a5407a3e4f6a050fbbea3
Victor Jack <67293106+thedon-dev@users.noreply.github.com>
2024-07-30 09:55:59 -0800

    About section fixes

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -6,12 +6,12 @@ import '../Styles/About.css'
 
 function About() {
   return (
-    <div className='min-h-[100vh] grid grid-cols-1 lg:grid-cols-2 gap-5 px-[8%]' id='about'>
+    <div className='min-h-[100vh] grid grid-cols-1 lg:grid-cols-2 gap-5 px-[8%] pb-10' id='about'>
         <div className='my-20 text-white'>
             <ul >
-                <li><h4 className='text-blue-500 text-xl'>:About Me</h4></li>
+                <li><h4 className='text-blue-500 text-lg lg:text-xl'>:About Me</h4></li>
             </ul>
-            <div className='mt-10'>
+            <div className='mt-10 text-xs lg:text-base'>
                 <p className='text-white'>Hey There! My name is Victor Jack I am a problem solver, a leader and a critical thinker,
                     I enjoy turning complex problems into beautiful designs. My tech journey started in 2020, with me messing around
                     different programming languages and making games. My passion for games and trying to make one introduced me to the Tech space.
@@ -33,7 +33,7 @@ function About() {
                 </div>
             </div>
         </div>
-        <div className='grid place-content-center'>
+        <div className='grid place-content-center px-10'>
             <div className='border-4 rounded-lg border-dotted border-white p-3'>
                 <div className='h-[350px] w-[350px] lg:h-[500px] lg:w-[500px] overflow-hidden group'>
                     <img src={Pic6} alt="" className='group-hover:scale-125 -translate-y-10 lg:-translate-y-20 transition-all duration-300'/>
