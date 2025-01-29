0ed3c273854b6c6c1f97ca44ec80bf14c65e4c3f
Victor Jack <67293106+thedon-dev@users.noreply.github.com>
2025-01-29 11:08:38 +0100

    Added new project

---
+++ src/Sections/digits.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const digits = () => {
+  return <div>digits</div>;
+};
+
+export default digits;
--- src/data/Projects.js
+++ src/data/Projects.js
@@ -1,40 +1,48 @@
-import Nke from '../assets/ProjectImages/Nke.png'
-import XploreAfrica from '../assets/ProjectImages/XploreAfrica.png'
-import NgGameDev from '../assets/ProjectImages/NGGameDev.png'
-import BabaAI from '../assets/ProjectImages/BabaAI.png'
+import Nke from "../assets/ProjectImages/Nke.png";
+import XploreAfrica from "../assets/ProjectImages/XploreAfrica.png";
+import NgGameDev from "../assets/ProjectImages/NGGameDev.png";
+import BabaAI from "../assets/ProjectImages/BabaAI.png";
+import jpegbyprince from "../assets/ProjectImages/jpegbyprince.png";
 
+const projects = [
+  {
+    name: "Nke Africa",
+    image: Nke,
+    description:
+      "Nke Africa is a story-telling platform, dedicated to telling authentic stories of African Women.",
+    technologies: ["ReactJS", "TailwindCSS"],
+    link: "https://nkeafrica.netlify.app",
+  },
+  {
+    name: "XploreAfrica",
+    image: XploreAfrica,
+    description: "XploreAfrica is a Software Development Agency.",
+    technologies: ["ReactJS", "TailwindCSS"],
+    link: "https://xploreafrica.vercel.app",
+  },
+  {
+    name: "NG.GameDev",
+    image: NgGameDev,
+    description:
+      "NG.GameDev is a vibrant Nigerian game community to bring creators in the game space together for collaboration and knowledge sharing.",
+    technologies: ["HTML", "CSS", "JavaScript"],
+    link: "https://ng-gamedev.netlify.app",
+  },
+  {
+    name: "Baba's AI",
+    image: BabaAI,
+    description:
+      "Baba's AI is an AI tool that helps you simplify task and boost productivity with adequate fluency in pidgin as well as your native tongue and English",
+    technologies: ["ReactJS", "TailwindCSS", "Express", "MongoDb"],
+    link: "https://baba-frontend-react.onrender.com/",
+  },
+  {
+    name: "Photographer Portfolio",
+    image: jpegbyprince,
+    description: "This is a one page portfolio for a Photographer",
+    technologies: ["ReactJS", "TailwindCSS"],
+    link: "https://jpegbyprince.vercel.app",
+  },
+];
 
-const projects =[
-    {
-        name: 'Nke Africa',
-        image: Nke,
-        description: 'Nke Africa is a story-telling platform, dedicated to telling authentic stories of African Women.',
-        technologies: ['ReactJS', 'TailwindCSS'],
-        link: 'https://nkeafrica.netlify.app'
-    },
-    {
-        name: 'XploreAfrica',
-        image: XploreAfrica,
-        description: 'XploreAfrica is a Software Development Agency.',
-        technologies: ['ReactJS', 'TailwindCSS'],
-        link: 'https://xploreafrica.vercel.app'
-    },
-    {
-        name: 'NG.GameDev',
-        image: NgGameDev,
-        description: 'NG.GameDev is a vibrant Nigerian game community to bring creators in the game space together for collaboration and knowledge sharing.',
-        technologies: ['HTML', 'CSS', 'JavaScript'],
-        link: 'https://ng-gamedev.netlify.app'
-    },
-    {
-        name: 'Baba\'s AI',
-        image: BabaAI,
-        description: 'Baba\'s AI is an AI tool that helps you simplify task and boost productivity with adequate fluency in pidgin as well as your native tongue and English',
-        technologies: ['ReactJS', 'TailwindCSS', 'Express', 'MongoDb'],
-        link: 'https://baba-frontend-react.onrender.com/'
-    },
-]
-
-
-
-export default projects
+export default projects;
